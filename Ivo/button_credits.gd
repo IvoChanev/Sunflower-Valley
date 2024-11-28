605ee81114c246ccf9e6ef46c82c0f256df3131e
IvoChanev <i.c.chanev@st.hanze.nl>
2024-11-28 10:45:32 +0200

    Menu Stuff
    - Scene transitions
    - Phone optimized
    - A few VFX scripts and shaders

---
+++ Ivo/button_credits.gd
@@ -0,0 +1,22 @@
+extends Sprite2D
+
+
+# Adjustable properties
+@export var new_texture: Texture2D  # The texture to change to on click
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			if _is_mouse_over():
+				_on_sprite_clicked()
+
+func _is_mouse_over() -> bool:
+	# Check if the mouse is over the sprite
+	var mouse_pos = get_global_mouse_position()
+	return get_rect().has_point(to_local(mouse_pos))
+
+func _on_sprite_clicked():
+	# Change the texture when clicked
+	if new_texture:
+		texture = new_texture
+		SceneTransition.change_scene("res://credits.tscn")
