f4d6ed9eddfc8b19c22eefb1284473c869a14145
Sonia Cailean <64458929+remiehzla@users.noreply.github.com>
2025-01-09 15:30:03 +0100

    End of day resume
    Blinder animation support
    Label displaying the score showing up based on dynamic results

--- assets/scripts/inspection_room.gd
+++ assets/scripts/inspection_room.gd
@@ -1,12 +1,16 @@
 extends Node
 
 @onready var plant_scene = preload("res://scenes/plant.tscn")
+@onready var animation_player = $Blinders/AnimationPlayer
+@onready var summary_label = $SummaryLabel
 
 var plant: Node2D = null
 var plant_type: String = ""
 var disease: String = ""
 
 func _ready():
+	summary_label.visible = false
+	
 	# Get plant and disease from PlantManager
 	var plant_info = PlantManager.current_plant_and_disease
 	if plant_info != null:
@@ -36,8 +40,24 @@ func _on_heal_button_pressed() -> void:
 	PlantManager.healed_plants_count += 1
 	$Plant.mark_as_healed()
 	
-# End the day
+# End the day 
 func _on_end_day_button_pressed() -> void:
+	animation_player.play("move_down")
+	animation_player.connect("animation_finished", Callable(self, "_on_blinders_animation_finished"))
+
+# Display day resume at the end of the animation
+func _on_blinders_animation_finished(animation_name: String):
+	if animation_name == "move_down":
+		display_day_summary()
+		
+func display_day_summary():
+	var healed = PlantManager.healed_plants_count
+	var killed = PlantManager.killed_plants_count
+	
+	#Make the label visible
+	summary_label.visible = true
+	summary_label.text = "Day Summary:\nHealed plants: %d\nKilled plants: %d" % [healed, killed]
+
 	var summary = "Day Summary:\n"
 	summary += "Healed plants: %d\n" % PlantManager.healed_plants_count
 	summary += "Killed plants: %d" % PlantManager.killed_plants_count
